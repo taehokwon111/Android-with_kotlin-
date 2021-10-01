1290f15f283ee6ceb9f44a9d98fcf9621a91e1ba
권태호 <taehokwon48@gmail.com>
2021-10-01 16:53:14 +0900

    password_setting

--- secret_flie/app/src/main/java/com/example/secret_flie/MainActivity.kt
+++ secret_flie/app/src/main/java/com/example/secret_flie/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.secret_flie
 
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.NumberPicker
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,6 +43,7 @@ class MainActivity : AppCompatActivity() {
     private val changeButton: AppCompatButton by lazy {
         findViewById<AppCompatButton>(R.id.changebtn)
     }
+    private var changePasswordMode = false;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -47,7 +53,67 @@ class MainActivity : AppCompatActivity() {
         numberPicker3
 
         openButton.setOnClickListener {
-            
+
+            if (changePasswordMode){
+                Toast.makeText(this, "비밀번호 변경중.", Toast.LENGTH_SHORT).show();
+                return@setOnClickListener;
+            }
+
+            val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE);
+
+            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}";
+
+            if(passwordPreferences.getString("password", "000").equals(passwordFromUser)){
+                //패스워드 성공
+                //TODO 다이어리 후 남겨줘야함
+                //startActivity()
+            }else {
+                showErrorDialog()
+                //실패
+            }
+        }
+
+        changeButton.setOnClickListener {
+            val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE);
+            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}";
+
+            if(changePasswordMode){
+                //번호를 저장하는 기능
+
+                passwordPreferences.edit(){
+                    putString("password", passwordFromUser);
+                }
+
+                changePasswordMode = false
+                changeButton.setBackgroundColor(Color.BLACK);
+
+            } else {
+                // changePasswordMode가 활성화 :: 비밀번호 유효성 체크
+
+                if(passwordPreferences.getString("password", "000").equals(passwordFromUser)){
+                    //패스워드 성공
+                    //TODO 다이어리 후 남겨줘야함
+
+                    changePasswordMode = true
+                    Toast.makeText(this, "변경할 비밀번호 입력", Toast.LENGTH_SHORT).show();
+
+                    changeButton.setBackgroundColor(Color.RED);
+
+                }else {
+                    showErrorDialog()
+                    //실패
+                }
+            }
         }
     }
+
+    private fun showErrorDialog(){
+        AlertDialog.Builder(this)
+            .setTitle("실패")
+            .setMessage("비밀번호 불일치")
+            .setPositiveButton("확인"){ _, _ -> }
+            .create()
+            .show()
+    }
+
 }
