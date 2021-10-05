168e03367d070da73e8a1daffb128a7bdb298c6b
권태호 <taehokwon48@gmail.com>
2021-10-05 15:39:18 +0900

    calc_design_complete

--- calc/app/src/main/java/com/example/calc/MainActivity.kt
+++ calc/app/src/main/java/com/example/calc/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.calc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnclick(v: View){
+        Toast.makeText(this, "응애", Toast.LENGTH_SHORT).show()
+    }
+    fun historybtnclick(v: View){
+        Toast.makeText(this, "응애", Toast.LENGTH_SHORT).show()
+    }
+    fun resultbtnclick(v: View){
+        Toast.makeText(this, "응애", Toast.LENGTH_SHORT).show()
+    }
+    fun clearbtnclick(v: View){
+        Toast.makeText(this, "응애", Toast.LENGTH_SHORT).show()
     }
 }
